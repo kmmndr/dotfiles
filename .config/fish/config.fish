f52f08dc64bfd99e9411fecd7d6d359ee1b70fc1
Thomas Kienlen <kommander@laposte.net>
2017-06-21 13:21:21 +0200

    fish aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,9 @@ set -x GOPATH ~/work/go
 set -x PATH $GOPATH/bin $PATH
 
 alias be='bundle exec'
+abbr --add be bundle exec
+abbr --add bi bundle install
+abbr --add bu bundle update
 alias g='git'
 alias t='tig'
 alias G='gvim'
@@ -16,6 +19,8 @@ alias dd_stats='killall dd -USR1'
 alias ssh_no_key='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
 alias pbcopy='xclip -selection c'
 alias pbpaste='xclip -selection clipboard -o'
+alias view='vim -R'
+alias tmuxd='tmux new-session -d'
 
 set -x EDITOR vim
 set -x PAGER less
